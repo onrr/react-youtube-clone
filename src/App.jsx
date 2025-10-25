668021c7be66932897f2b0a8620b3cda945621ec
onrr <spr1777@hotmail.com>
2025-10-25 16:17:02 +0300

    tailwind configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import YoutubeLogo from './components/YoutubeLogo.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <YoutubeLogo />
+    <a href="#">link test</a>
+      <p className='color-primary'>test</p>
+      <h1>test h1</h1>
+      <h2>test h2</h2>
+      <h3>test h3</h3>
+      <h4>test h4</h4>
+      <h5>test h5</h5>
+      <h6>test h6</h6>
     </>
   )
 }
